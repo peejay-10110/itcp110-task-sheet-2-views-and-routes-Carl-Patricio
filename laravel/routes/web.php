fa682a12c297a514705ec9b37490a35275b509d0
Just-SleepPls <patriciocarl235@gmail.com>
2026-05-06 23:42:16 +0800

    Add files via upload

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PortfolioController::class, 'home'])->name('home');
+Route::get('/about', [PortfolioController::class, 'about'])->name('about');
+Route::get('/skills', [PortfolioController::class, 'skills'])->name('skills');
+Route::get('/projects', [PortfolioController::class, 'projects'])->name('projects');
+Route::get('/projects/{id}', [PortfolioController::class, 'projectShow'])->name('projects.show');
+Route::get('/experience', [PortfolioController::class, 'experience'])->name('experience');
+Route::get('/education', [PortfolioController::class, 'education'])->name('education');
+Route::get('/blog', [PortfolioController::class, 'blog'])->name('blog');
+Route::get('/blog/{slug}', [PortfolioController::class, 'blogShow'])->name('blog.show');
+Route::get('/contact', [PortfolioController::class, 'contact'])->name('contact');
+Route::post('/contact', [PortfolioController::class, 'contactSend'])->name('contact.send');
